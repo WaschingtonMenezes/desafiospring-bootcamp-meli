4c8923242c6b95a8d71ac4230b8fd09765b2b4b1
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-06 16:57:32 -0300

    chore: add dependecies

---
+++ desafio_spring/src/main/java/com/desafiospringboot/DesafioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.desafiospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSpringBootApplication.class, args);
+    }
+
+}
---
+++ desafio_spring/src/test/java/com/desafiospringboot/DesafioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
