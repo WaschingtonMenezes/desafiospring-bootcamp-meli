9026865bec93fe472b70b929dbe288bd03d73d25
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-06 17:41:25 -0300

    feat: add userEntity

---
+++ desafio_spring/src/main/java/com/desafiospringboot/Entities/User.java
@@ -0,0 +1,41 @@
+package com.desafiospringboot.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotNull
+    private String name;
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
