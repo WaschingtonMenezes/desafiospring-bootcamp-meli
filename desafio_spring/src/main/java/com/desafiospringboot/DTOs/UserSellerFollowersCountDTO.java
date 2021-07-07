72a9c0f57c3f369a127a5a2404a45b7306a94590
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 15:32:46 -0300

    feat: implements followers count

---
+++ desafio_spring/src/main/java/com/desafiospringboot/DTOs/UserSellerFollowersCountDTO.java
@@ -0,0 +1,22 @@
+package com.desafiospringboot.DTOs;
+
+import com.desafiospringboot.Entities.UserSeller;
+
+public class UserSellerFollowersCountDTO {
+    private int userId;
+    private String name;
+    private int count;
+
+    public UserSellerFollowersCountDTO(int userId, String name, int count) {
+        this.userId = userId;
+        this.name = name;
+        this.count = count;
+    }
+
+    public UserSellerFollowersCountDTO() {
+    }
+
+    public static UserSellerFollowersCountDTO convert(UserSeller seller) {
+        return new UserSellerFollowersCountDTO(seller.getId(), seller.getName(), (int) seller.getFollowers().stream().count());
+    }
+}
--- desafio_spring/src/main/java/com/desafiospringboot/Service/UserSellerService.java
+++ desafio_spring/src/main/java/com/desafiospringboot/Service/UserSellerService.java
@@ -1,15 +1,28 @@
 package com.desafiospringboot.Service;
 
+import com.desafiospringboot.DTOs.UserSellerFollowersCountDTO;
+import com.desafiospringboot.Entities.UserSeller;
+import com.desafiospringboot.Exception.UserNotFoundException;
 import com.desafiospringboot.Repositories.UserClientRepository;
+import com.desafiospringboot.Repositories.UserSellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserSellerService {
-    private UserClientRepository userClientRepository;
+    private UserSellerRepository userSellerRepository;
 
     @Autowired
-    public UserSellerService(UserClientRepository userClientRepository) {
-        this.userClientRepository = userClientRepository;
+    public UserSellerService(UserSellerRepository repository) {
+        this.userSellerRepository = repository;
+    }
+
+    public UserSellerFollowersCountDTO countFollowers(int userId) {
+        UserSeller seller = this.userSellerRepository.findById(userId).stream().findFirst().orElse(null);
+        if(seller == null) {
+            throw new UserNotFoundException("Vendedor não encontrado");
+        }
+
+        return UserSellerFollowersCountDTO.convert(seller);
     }
 }
