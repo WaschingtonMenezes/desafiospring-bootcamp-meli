5c9728b81dd85c2b0e3920fa34ba1b5484e02984
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 10:26:32 -0300

    fix: rename route

--- desafio_spring/src/main/java/com/desafiospringboot/Controllers/UserClient.java
+++ desafio_spring/src/main/java/com/desafiospringboot/Controllers/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("userClient")
+@RequestMapping("/userClients")
 public class UserClient {
     private UserClientService userClientService;
 
