e01c4df30bce32d4bc2f1cac3db802a0806feb31
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 09:48:29 -0300

    feat: add userSellerRepository

---
+++ desafio_spring/src/main/java/com/desafiospringboot/Repositories/UserSellerRepository.java
@@ -0,0 +1,9 @@
+package com.desafiospringboot.Repositories;
+
+import com.desafiospringboot.Entities.UserSeller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserSellerRepository extends JpaRepository<UserSeller, Long> {
+}
