47cb2d4605eaaa18bfd6eb47bb79e356b004d8f6
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 10:02:23 -0300

    feat: add ProductController

---
+++ desafio_spring/src/main/java/com/desafiospringboot/Controllers/ProductController.java
@@ -0,0 +1,17 @@
+package com.desafiospringboot.Controllers;
+
+import com.desafiospringboot.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+}
