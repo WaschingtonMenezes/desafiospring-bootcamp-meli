e20ce7cd990ef168899ddd985cf6cb4fa7f5e5e1
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 10:18:16 -0300

    fix: rename UserSellerService

--- desafio_spring/src/main/java/com/desafiospringboot/Service/UserSellerService.java
+++ desafio_spring/src/main/java/com/desafiospringboot/Service/UserSellerService.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserClientSeller {
+public class UserSellerService {
     private UserClientRepository userClientRepository;
 
     @Autowired
-    public UserClientSeller(UserClientRepository userClientRepository) {
+    public UserSellerService(UserClientRepository userClientRepository) {
         this.userClientRepository = userClientRepository;
     }
 }
