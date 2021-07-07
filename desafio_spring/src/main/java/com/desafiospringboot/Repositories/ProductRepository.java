f5be89298e72d7475eb40733a46cea72a434f540
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 09:43:57 -0300

    fix: change Entity name from JpaRepository

--- desafio_spring/src/main/java/com/desafiospringboot/Repositories/ProductRepository.java
+++ desafio_spring/src/main/java/com/desafiospringboot/Repositories/ProductRepository.java
@@ -1,8 +1,9 @@
 package com.desafiospringboot.Repositories;
 
+import com.desafiospringboot.Entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductRepository, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
