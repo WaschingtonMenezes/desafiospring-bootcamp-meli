ffcdb8a36d66727b5d99b1f05c19cb32130ea217
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 09:54:32 -0300

    feat: add productService

---
+++ desafio_spring/src/main/java/com/desafiospringboot/Service/ProductService.java
@@ -0,0 +1,15 @@
+package com.desafiospringboot.Service;
+
+import com.desafiospringboot.Repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
