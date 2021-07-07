020aaecf577141602b951ddcdbe9f707643400de
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 10:22:22 -0300

    feat: add userClientController

---
+++ desafio_spring/src/main/java/com/desafiospringboot/Controllers/UserClient.java
@@ -0,0 +1,17 @@
+package com.desafiospringboot.Controllers;
+
+import com.desafiospringboot.Service.UserClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("userClient")
+public class UserClient {
+    private UserClientService userClientService;
+
+    @Autowired
+    public UserClient(UserClientService userClientService) {
+        this.userClientService = userClientService;
+    }
+}
