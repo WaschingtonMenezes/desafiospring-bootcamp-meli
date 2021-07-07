c414a638325795a5e2515c095484fe5d2e92bcb2
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-07 09:43:15 -0300

    chore: add maven dependencies

--- desafio_spring/src/main/java/com/desafiospringboot/DesafioSpringBootApplication.java
+++ desafio_spring/src/main/java/com/desafiospringboot/DesafioSpringBootApplication.java
@@ -1,13 +1,13 @@
 package com.desafiospringboot;
 
+import com.desafiospringboot.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesafioSpringBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DesafioSpringBootApplication.class, args);
     }
-
 }
