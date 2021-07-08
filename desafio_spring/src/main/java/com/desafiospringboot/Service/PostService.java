f324fee3b1d4556f1a7a4a26db3489b19a7198b9
icrxz <igor.cruz@mercadolivre.com>
2021-07-08 11:57:37 -0300

    feat: create PostService and add method to create Post

--- desafio_spring/src/main/java/com/desafiospringboot/Service/PostService.java
+++ desafio_spring/src/main/java/com/desafiospringboot/Service/PostService.java
@@ -1,5 +1,9 @@
 package com.desafiospringboot.Service;
 
+import com.desafiospringboot.DTOs.PostDTO;
+import com.desafiospringboot.Entities.Post;
+import com.desafiospringboot.Entities.Product;
+import com.desafiospringboot.Entities.UserSeller;
 import com.desafiospringboot.Repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,9 +11,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class PostService {
     private PostRepository postRepository;
+    private ProductService productService;
+    private UserSellerService userSellerService;
 
     @Autowired
-    PostService(PostRepository repository) {
+    PostService(PostRepository repository, ProductService productService, UserSellerService userSellerService) {
         postRepository = repository;
+        this.productService = productService;
+        this.userSellerService = userSellerService;
+    }
+
+    public Post createPost(PostDTO postDTO) {
+        Post post = PostDTO.convert(postDTO);
+        UserSeller user = userSellerService.findUserSellerById(postDTO.getUserId());
+        Product createdProduct = productService.findOrCreateProduct(postDTO.getDetail());
+
+        post.setDetail(createdProduct);
+        post.setUserSeller(user);
+
+        return postRepository.save(post);
     }
 }
