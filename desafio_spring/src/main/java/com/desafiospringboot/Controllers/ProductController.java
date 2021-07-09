a9126038aad722241175772f81e32a8226b0e895
icrxz <igor.cruz@mercadolivre.com>
2021-07-09 14:39:03 -0300

    feat: add endpoint to get post list from followed sellers

--- desafio_spring/src/main/java/com/desafiospringboot/Controllers/ProductController.java
+++ desafio_spring/src/main/java/com/desafiospringboot/Controllers/ProductController.java
@@ -1,13 +1,11 @@
 package com.desafiospringboot.Controllers;
 
+import com.desafiospringboot.DTOs.UserFollowedPostsDTO;
 import com.desafiospringboot.DTOs.Post.PostDTO;
 import com.desafiospringboot.DTOs.Post.PostPromoCountDTO;
 import com.desafiospringboot.DTOs.Post.PostPromoDTO;
-import com.desafiospringboot.DTOs.UserSeller.UserSellerFollowedDTO;
 import com.desafiospringboot.Entities.Post;
-import com.desafiospringboot.Entities.UserClient;
 import com.desafiospringboot.Service.PostService;
-import com.desafiospringboot.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +16,11 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    private ProductService productService;
-    private PostService postService;
+    private final PostService postService;
 
     @Autowired
-    public ProductController(ProductService productService, PostService postService) {
+    public ProductController(PostService postService) {
         this.postService = postService;
-        this.productService = productService;
     }
 
     @PostMapping("/newpost")
@@ -37,12 +33,21 @@ public class ProductController {
     @PostMapping("/newpromopost")
     public ResponseEntity<PostPromoDTO> createPromoPost(@RequestBody @Valid PostPromoDTO newPost) {
         Post createdPost = postService.createPromoPost(newPost);
+
         return ResponseEntity.ok(PostPromoDTO.convert(createdPost));
     }
 
     @GetMapping("/{userId}/countPromo")
     public ResponseEntity<PostPromoCountDTO> getPostPromo(@PathVariable int userId) {
         PostPromoCountDTO promo =  this.postService.getPostPromoCount(userId);
+
         return new ResponseEntity<>(promo, HttpStatus.OK);
+    }
+
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity<UserFollowedPostsDTO> getUserFollowedPosts(@PathVariable int userId) {
+        UserFollowedPostsDTO followedPostsDTO = this.postService.getPostsFromFollowedUsers(userId);
+
+        return ResponseEntity.ok(followedPostsDTO);
     }
 }
