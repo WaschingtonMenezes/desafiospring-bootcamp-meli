93f7cad2c640c4f453b49e37047e0bade4d31e4b
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-07-09 12:37:39 -0300

    feat: implements getters and setters

--- desafio_spring/src/main/java/com/desafiospringboot/Entities/UserSeller.java
+++ desafio_spring/src/main/java/com/desafiospringboot/Entities/UserSeller.java
@@ -2,7 +2,6 @@ package com.desafiospringboot.Entities;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.List;
 
@@ -28,5 +27,13 @@ public class UserSeller extends User{
 
     public void setFollowers(List<UserClient> followers) {
         this.followers = followers;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 }
