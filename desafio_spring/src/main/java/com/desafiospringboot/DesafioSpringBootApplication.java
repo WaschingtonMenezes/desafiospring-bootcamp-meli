15175f93f617c94b7e410e62ddef07448d817802
icrxz <igor.cruz@mercadolivre.com>
2021-07-11 20:46:49 -0300

    refactor: Remove main application unused imports

--- desafio_spring/src/main/java/com/desafiospringboot/DesafioSpringBootApplication.java
+++ desafio_spring/src/main/java/com/desafiospringboot/DesafioSpringBootApplication.java
@@ -1,7 +1,5 @@
 package com.desafiospringboot;
 
-import com.desafiospringboot.Repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
