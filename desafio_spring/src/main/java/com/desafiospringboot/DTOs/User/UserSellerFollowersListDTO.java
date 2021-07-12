29e3c7c73d053110ca3814e989fe224464692d31
icrxz <igor.cruz@mercadolivre.com>
2021-07-11 21:59:46 -0300

    refactor: Change UserSellerFollowersListDTO getters/setters

--- desafio_spring/src/main/java/com/desafiospringboot/DTOs/User/UserSellerFollowersListDTO.java
+++ desafio_spring/src/main/java/com/desafiospringboot/DTOs/User/UserSellerFollowersListDTO.java
@@ -10,27 +10,38 @@ public class UserSellerFollowersListDTO {
     @NotNull
     private int userId;
     @NotNull
-    private String sellerName;
+    private String userName;
     private List<UserDTO> followers;
 
-    public UserSellerFollowersListDTO(int userId,String sellerName, List<UserDTO> followers) {
-		
-		this.userId = userId;
-		this.sellerName = sellerName;
-		this.followers = followers;
-	}
-
-	public int getUserId() {
+    public int getUserId() {
         return userId;
     }
 
-    public String getSellerName() {
-        return sellerName;
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public List<UserDTO> getFollowers() {
         return followers;
     }
+
+    public void setFollowers(List<UserDTO> followers) {
+        this.followers = followers;
+    }
+
+    public UserSellerFollowersListDTO(int userId, String sellerName, List<UserDTO> followers) {
+		this.userId = userId;
+		this.userName = sellerName;
+		this.followers = followers;
+	}
 
     public static UserSellerFollowersListDTO convert (List<UserClient> listClients, UserSeller seller) {
     	return new UserSellerFollowersListDTO(seller.getId(), seller.getName(), UserDTO.convert(listClients));
