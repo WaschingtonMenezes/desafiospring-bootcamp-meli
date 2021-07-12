7291a55e311d62e044fab4b92e7012028df087ba
icrxz <igor.cruz@mercadolivre.com>
2021-07-11 21:58:56 -0300

    refactor: Change UserSellerFollowersCountDTO getters/setters

--- desafio_spring/src/main/java/com/desafiospringboot/DTOs/User/UserSellerFollowersCountDTO.java
+++ desafio_spring/src/main/java/com/desafiospringboot/DTOs/User/UserSellerFollowersCountDTO.java
@@ -11,6 +11,30 @@ public class UserSellerFollowersCountDTO {
     private String userName;
     private int followers_count;
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getFollowers_count() {
+        return followers_count;
+    }
+
+    public void setFollowers_count(int followers_count) {
+        this.followers_count = followers_count;
+    }
+
     public UserSellerFollowersCountDTO(int userId, String userName, int followers_count) {
         this.userId = userId;
         this.userName = userName;
@@ -22,17 +46,5 @@ public class UserSellerFollowersCountDTO {
 
     public static UserSellerFollowersCountDTO convert(UserSeller seller) {
         return new UserSellerFollowersCountDTO(seller.getId(), seller.getName(), seller.getFollowers().size());
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public String getName() {
-        return userName;
-    }
-
-    public int getCount() {
-        return followers_count;
     }
 }
